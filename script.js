6285665cc9ae51cb59e68feda15df0c5c371dbfc
Venancius Cássio <112889934+venanciusdev@users.noreply.github.com>
2024-07-15 19:05:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.add-to-cart');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Produto adicionado ao carrinho!');
+        });
+    });
+});
